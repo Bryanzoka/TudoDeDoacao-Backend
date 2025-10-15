82df64891d130edb40fbc0a47cb00b77ade807b2
iagoliziero <iagolizierox14@gmail.com>
2025-10-14 22:26:04 -0300

    wip: creating email verification code page

--- resources/views/emails/email.blade.php
+++ resources/views/emails/email.blade.php
@@ -5,6 +5,9 @@
     <title>Verifique seu E-mail</title>
 </head>
 <style>
+
+    {/* Tags */}
+
     html, body {
         height: 100%;
         overflow: hidden;
@@ -13,12 +16,19 @@
 
     body {
         font-family: gotham, arial, helvetica, sans-serif;
-        font-size: 18px;
         padding: 0;
         margin: 0 auto !important;
         display: flex;
         flex-direction: column;
-        justify-content: space-between;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 20px;
     }
 
     main {
@@ -29,64 +39,78 @@
             width: 100vw;
         }
 
-        .container-title__main {
-            display: flex;
-            justify-content: center; 
-            flex-direction: column;
-            align-items: center;
-        }
+    h1, h2, p {
+        margin: 0; 
+        color: #080402;
+    }
 
-        .section__main {
-            display: flex;
-            justify-content: center;
-            flex-direction: column;
-        }
+    img:nth-child(3) {
+        transform: rotate(180deg)
+    }
 
-        .div-images__main {
-            display: flex;
-            justify-content: center; 
-            align-items: center;
-            gap: 20px;
-        }
 
-        img:nth-child(3) {
-            transform: rotate(180deg)
-        }
+    {/* Class */}
 
-        .div-carta__section {
-            display: flex;
-            justify-content: center; 
-            align-items: center;
-            position: relative;
-            top: 40px;
-        }
+    .container-title__main {
+        display: flex;
+        justify-content: center; 
+        flex-direction: column;
+        align-items: center;
+        margin: 50px 0 20px 0;
+    }
 
-        .img__main {
-            width: 250px;
-            height: auto;
-        }
+    .section__main {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
 
-        h1 {
-                margin: 0; 
-                font-size: 24px;
-                color: #080402;
-        }
+    .div-images__main {
+        display: flex;
+        justify-content: center; 
+        align-items: center;
+        gap: 20px;
+    }
 
-        .p__main {
-            margin: 10px 0 30px;
-            font-size: 16px;
-            color: #080402;
-            max-width: 450px;
-            text-align: center;
-        }
+    .div-carta__section {
+        display: flex;
+        justify-content: center; 
+        align-items: center;
+        position: relative;
+        top: 40px;
+    }
 
+    .code__container-title {
+        width: 250px;
+        height: auto;
+        background-color: #FFCEE0;
+        border-radius: 15px;
+
+        & .h1__code {
+            padding: 10px;
+            letter-spacing: 5px;
+            font-size: 2.5rem;
+        }
+    }
+
+    .img__main {
+        width: 250px;
+        height: auto;
+    }
+
+    .p__main {
+        margin: 10px 0 30px;
+        font-size: 18px;
+        max-width: 450px;
+        text-align: center;
+    }
 </style>
 <body>
     <header> 
-        logo
+        <img src="{{ asset('images/Logo.png') }}" alt="Logo" class="">
     </header>
     <main> 
-        <div style="padding: 40px 20px; text-align: center; color: white; width: 100vw;">
+        <div style=" text-align: center; color: white; width: 100vw;">
 
         <section class="section__main"> 
             <div class="div-carta__section">
@@ -94,41 +118,28 @@
             </div>
                 <!-- Título -->
             <div class="div-images__main">
-                <img src="{{ asset('images/blossom-flower.png') }}" alt="Email Icon" class="img__main">
+                <img src="{{ asset('images/blossom-flower.png') }}" alt="Flor de Cerejeira" class="img__main">
 
                 <div class="container-title__main"> 
-                    <h1 >Verify Your Email Account</h1>
+                    <h2>Verify Your Email Account</h2>
         
                 <!-- Texto descritivo -->
                 <p class="p__main">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.
                 </p>
+                    <div class="code__container-title">
+                        <h1 class="h1__code"> 1245642 </h1>
+                    </div>
                 </div>
 
-                <img src="{{ asset('images/blossom-flower.png') }}" alt="Email Icon"  class="img__main">
+                <img src="{{ asset('images/blossom-flower.png') }}" alt="Flor de Cerejeira"  class="img__main">
             </div>
         </section>
 
         <!-- Botão de verificação -->
+
     </div>
     </main>
-
-    <footer> 
-        <!-- Rodapé -->
-        <div style="background-color: #f4f4f4; padding: 30px 20px; text-align: center;">
-            <!-- Ícones sociais (substitua os hrefs pelos links reais) -->
-            <div style="margin-bottom: 15px;">
-                <a href="#"><img src="https://img.icons8.com/ios-filled/24/000000/facebook.png" alt="Facebook" style="margin: 0 10px;"></a>
-                <a href="#"><img src="https://img.icons8.com/ios-filled/24/000000/instagram-new.png" alt="Instagram" style="margin: 0 10px;"></a>
-                <a href="#"><img src="https://img.icons8.com/ios-filled/24/000000/twitter.png" alt="Twitter" style="margin: 0 10px;"></a>
-                <a href="#"><img src="https://img.icons8.com/ios-filled/24/000000/pinterest--v1.png" alt="Pinterest" style="margin: 0 10px;"></a>
-            </div>
-
-            <!-- Informações de contato -->
-            <p style="margin: 5px 0; font-size: 14px; color: #333;">(738) 479-6719</p>
-            <p style="margin: 5px 0; font-size: 14px; color: #333;">info@website.com - Simple Pleb</p>
-        </div>
-    </footer>
 
 </body>
 </html>
