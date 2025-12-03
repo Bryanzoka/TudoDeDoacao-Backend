6093348337c752ad08c59de067c98dd16a7e6171
unknown <bryantbsalesjob@gmail.com>
2025-12-03 17:01:01 -0300

    fix: correcting route conflict

--- routes/api.php
+++ routes/api.php
@@ -23,27 +23,27 @@ Route::middleware(['jwt.auth', 'role'])->group(function() {
     Route::delete('users/{id}', [UserController::class, 'destroy']);
 });
 
-Route::get('/donations/search', [DonationController::class, 'getFiltered']);
 Route::apiResource('/donations', DonationController::class)->except([
     'create',
     'edit'
-])->middleware('jwt.auth');
-
-
-Route::post('/auth/request-verification-code', [AuthController::class, 'requestCode']);
-
-Route::get('/donations/location/{location}', [DonationController::class, 'getByLocation']);
-
-Route::get('/donations/category/{category}', [DonationController::class, 'getByCategory']);
-
-Route::get('/donations/search/{name}', [DonationController::class, 'getByName']);
-
-Route::middleware('jwt.auth')->group(function() {
-    Route::get('/users/{id}/donations', [DonationController::class, 'getByUser']);
-    Route::get('/donations/location', [DonationController::class, 'getByMyLocation']);
-    Route::get('/donations/favorites', [FavoriteController::class, 'myFavorites']);
-    Route::post('/donations/favorites/{donation}', [FavoriteController::class, 'favorite']);
-    Route::delete('/donations/favorites/{donation}', [FavoriteController::class, 'unfavorite']);
+    ])->middleware('jwt.auth');
+    
+    
+    Route::post('/auth/request-verification-code', [AuthController::class, 'requestCode']);
+    
+    Route::get('/donations/location/{location}', [DonationController::class, 'getByLocation']);
+    
+    Route::get('/donations/category/{category}', [DonationController::class, 'getByCategory']);
+    
+    Route::get('/donations/search/{name}', [DonationController::class, 'getByName']);
+    
+    Route::middleware('jwt.auth')->group(function() {
+        Route::get('/users/{id}/donations', [DonationController::class, 'getByUser']);
+        Route::get('/donations/location', [DonationController::class, 'getByMyLocation']);
+        Route::get('/donations/favorites', [FavoriteController::class, 'myFavorites']);
+        Route::post('/donations/favorites/{donation}', [FavoriteController::class, 'favorite']);
+        Route::delete('/donations/favorites/{donation}', [FavoriteController::class, 'unfavorite']);
+        Route::get('/donations/search', [DonationController::class, 'getFiltered']);
 
     Route::get('/messages', [MessageController::class, 'index']);
     Route::post('/messages', [MessageController::class, 'store']);
