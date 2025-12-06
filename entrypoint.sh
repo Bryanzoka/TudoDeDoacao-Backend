ef73838fcf7cc5164c33fd67e9452131bc24e882
Bryan <bryantbsalesjob@gmail.com>
2025-12-05 21:53:21 -0300

    feat: add migration force in artisan migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,10 @@ if [ -z "$JWT_SECRET" ]; then
     php artisan jwt:secret --force
 fi
 
-php artisan migrate --force || true
+echo "Attempting to reset verification_codes table..."
+php artisan migrate:rollback --path=/database/migrations/2025_01_01_create_verification_codes_table.php || true
+
+php artisan migrate --force
 
 php artisan storage:link || true
 
